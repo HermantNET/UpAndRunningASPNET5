28bed856301cf6a507b4c7dc4ff61cde0718a2f1
Thomas <wolfershd@gmail.com>
2016-06-03 13:24:04 -0400

    Presentation

--- src/MealPlanApp/Controllers/FoodController.cs
+++ src/MealPlanApp/Controllers/FoodController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MealPlanApp.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -51,16 +52,21 @@ namespace MealPlanApp.Controllers
 
         public IActionResult Food(long FoodID)
         {
-            var food = _dataContext.Foods.Single(x => x.FoodID == FoodID);
-            _dataContext.Foods.Remove(food);
+            var food = _dataContext.Foods.Where(x => x.FoodID == FoodID);
 
-            return RedirectToAction("Index");
+            return View(food);
         }
 
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
         public IActionResult Delete(long FoodID)
         {
-            var toBeDeleted = _dataContext.Foods.Single(x => x.FoodID == FoodID);
-            toBeDeleted.
+            var foo = _dataContext.Foods.Single(x => x.FoodID == FoodID);
+            _dataContext.Foods.Remove(foo);
+            _dataContext.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
     }
